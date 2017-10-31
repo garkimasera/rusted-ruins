1d70b1e95de0f654fa0f9055a547bedb9c3e065b
T. Okubo <t.okubo.rx78+devel@gmail.com>
2017-10-31 21:31:34 +0900

    Some changes for damage processing

--- rusted-ruins/src/game/chara/mod.rs
+++ rusted-ruins/src/game/chara/mod.rs
@@ -1,4 +1,12 @@
 
 pub mod creation;
 
+use super::Game;
+use common::gamedata::chara::CharaId;
+
+pub fn damage(game: &mut Game, target: CharaId, damage: i32) {
+    let target = game.gd.chara.get_mut(target);
     
+    target.hp -= damage;
+}
+
--- rusted-ruins/src/game/combat/mod.rs
+++ rusted-ruins/src/game/combat/mod.rs
@@ -5,11 +5,11 @@ use common::objholder::AnimImgIdx;
 use common::gamedata::chara::CharaId;
 use rand::{thread_rng, Rng};
 
-pub fn attack_neighbor(game: &mut Game, chara_a: CharaId, chara_b: CharaId) {
-
-    let d = {
-        let attacker = game.gd.chara.get(chara_a);
-        let target = game.gd.chara.get(chara_b);
+pub fn attack_neighbor(game: &mut Game, attacker: CharaId, target: CharaId) {
+    // Damage calculation
+    let damage = {
+        let attacker = game.gd.chara.get(attacker);
+        let target = game.gd.chara.get(target);
         let mut attack_power = attacker.params.str as f64;
         let defence_power = target.params.vit as f64 / 2.0;
         attack_power *= thread_rng().gen_range(0.5, 1.0);
@@ -17,18 +17,16 @@ pub fn attack_neighbor(game: &mut Game, chara_a: CharaId, chara_b: CharaId) {
         let damage = attack_power - defence_power;
         if damage < 0.0 { 0 }else{ damage as i32 }
     };
-
-    damage(game, chara_a, chara_b, d);
-    
+    // Damage processing
+    super::chara::damage(game, target, damage);
+    // Logging
+    {
+        let attacker = game.gd.chara.get(attacker);
+        let target = game.gd.chara.get(target);
+        game_log!("attack"; attacker=attacker.name, target=target.name, damage=damage);
+    }
+    // Animation pushing
     game.anim_queue.push_back(Animation::img_onetile(
-        AnimImgIdx(0), game.gd.get_current_map().chara_pos(chara_b).unwrap()));
-}
-
-pub fn damage(game: &mut Game, attacker: CharaId, target: CharaId, damage: i32) {
-    let attacker_name = game.gd.chara.get(attacker).name.clone();;
-    let target = game.gd.chara.get_mut(target);
-    
-    target.hp -= damage;
-    game_log!("attack"; attacker=attacker_name, target=target.name, damage=damage);
+        AnimImgIdx(0), game.gd.get_current_map().chara_pos(target).unwrap()));
 }
 
