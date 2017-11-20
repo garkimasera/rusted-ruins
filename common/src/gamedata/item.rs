eeb5faea9c56bf7165446e0be57509ad8cec7933
T. Okubo <t.okubo.rx78+devel@gmail.com>
2017-11-20 21:10:41 +0900

    Add ItemList filtering routine

--- common/src/gamedata/item.rs
+++ common/src/gamedata/item.rs
@@ -26,6 +26,15 @@ pub enum ItemContent {
     },
 }
 
+impl ItemContent {
+    pub fn kind(&self) -> ItemKind {
+        match *self {
+            ItemContent::Object => ItemKind::Object,
+            ItemContent::Potion { .. } => ItemKind::Potion,
+        }
+    }
+}
+
 #[repr(u32)]
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Serialize, Deserialize)]
 pub enum PotionKind {
@@ -75,6 +84,67 @@ impl ItemList {
     /// Return item iterator
     pub fn iter(&self) -> ::std::slice::Iter<(Box<Item>, u32)> {
         self.items.iter()
+    }
+}
+
+/// Used for creating filtered list and saving filtering state
+#[derive(Clone, Debug)]
+pub struct ItemFilter {
+    pub all: bool,
+    pub kind: Option<ItemKind>,
+}
+
+impl ItemFilter {
+    /// Given item will be filtered (false) or not (true)
+    pub fn judge(&self, item: &Item) -> bool {
+        if self.all { return true; }
+        
+        if let Some(kind) = self.kind {
+            if item.content.kind() != kind { return false; }
+        }
+        
+        true
+    }
+}
+
+pub struct FilteredItemList<'a> {
+    item_list: &'a ItemList,
+    filter: ItemFilter,
+    count: usize,
+}
+
+impl<'a> FilteredItemList<'a> {
+    pub fn new(item_list: &'a ItemList, filter: ItemFilter) -> FilteredItemList<'a> {
+        FilteredItemList {
+            item_list, filter, count: 0,
+        }
+    }
+
+    pub fn all(item_list: &'a ItemList) -> FilteredItemList<'a> {
+        let filter = ItemFilter {
+            all: true, kind: None,
+        };
+        FilteredItemList::new(item_list, filter)
+    }
+}
+
+impl<'a> Iterator for FilteredItemList<'a> {
+    type Item = (usize, &'a Item, u32);
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.item_list.items.len() <= self.count {
+                return None
+            }
+            let a = &self.item_list.items[self.count];
+
+            let prev_count = self.count;
+            self.count += 1;
+
+            if self.filter.judge(&*a.0) {
+                return Some((prev_count, &*a.0, a.1));
+            }
+        }
     }
 }
 
