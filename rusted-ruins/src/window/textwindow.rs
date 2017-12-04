280b861d78b33157743eae89f33ce8bbbb4d416f
T. Okubo <t.okubo.rx78+devel@gmail.com>
2017-12-04 23:24:08 +0900

    Add some function to TextWindow

--- rusted-ruins/src/window/textwindow.rs
+++ rusted-ruins/src/window/textwindow.rs
@@ -1,14 +1,19 @@
 
 use super::commonuse::*;
+use super::widget::*;
+use sdlvalues::FontKind;
 
 pub struct TextWindow {
     rect: Rect,
+    label: LabelWidget,
 }
 
 impl TextWindow {
     pub fn new(rect: Rect) -> TextWindow {
         TextWindow {
             rect,
+            label: LabelWidget::wrapped(
+                (0, 0, rect.w as u32, 0), "", FontKind::M, rect.w as u32),
         }
     }
 }
@@ -18,7 +23,9 @@ impl Window for TextWindow {
     fn redraw(
         &mut self, canvas: &mut WindowCanvas, _game: &Game, sv: &mut SdlValues,
         _anim: Option<(&Animation, u32)>) {
-        
+
+        draw_rect_border(canvas, self.rect);
+        self.label.draw(canvas, sv);
     }
 }
 
