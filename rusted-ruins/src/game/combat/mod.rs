0eca8d2b513f93f10a27c02a15a69127d48fa618
T. Okubo <t.okubo.rx78+devel@gmail.com>
2018-04-11 22:27:00 +0900

    Rewrite damage calculation

--- rusted-ruins/src/game/combat/mod.rs
+++ rusted-ruins/src/game/combat/mod.rs
@@ -14,19 +14,31 @@ pub enum DamageKind {
     Poison,
 }
 
+/// Attack neighbor enemy by short range weapon or bare hands
 pub fn attack_neighbor(game: &mut Game, attacker: CharaId, target: CharaId) {
     // Damage calculation
-    let (damage, weapon_kind) = {
+    let damage = {
         let attacker = game.gd.chara.get(attacker);
         let target = game.gd.chara.get(target);
-        let weapon_data = get_weapon_data(attacker);
-        let weapon_dice_result = rng::dice(weapon_data.dice_n, weapon_data.dice_x);
+        
+        if let Some(weapon) = attacker.equip.item(EquipSlotKind::ShortRangeWeapon, 0) {
+            let weapon_obj = gobj::get_obj(weapon.idx);
+            let kind = get_weapon_kind(weapon_obj);
+            
+            let dice_result = rng::dice(weapon_obj.dice_n as i32, weapon_obj.dice_x as i32);
+            let damage_coef = 256 + attacker.params.str as i32 * 16;
+            let damage = dice_result.saturating_mul(damage_coef) / 256;
+            let defence_power = calc_defence(target);
 
-        let damage_coef = 256 + attacker.params.str as i32 * 16;
+            if damage < defence_power { 0 } else { damage - defence_power }
+        } else { // Attack by bare hands
+            let dice_result = rng::dice(1, 1);
+            let damage_coef = 256 + attacker.params.str as i32 * 16;
+            let damage = dice_result.saturating_mul(damage_coef) / 256;
+            let defence_power = calc_defence(target);
 
-        let damage = weapon_dice_result.saturating_mul(damage_coef) / 256;
-        let defence_power = calc_defence(target);
-        (if damage < defence_power { 0 }else{ damage - defence_power }, weapon_data.kind)
+            if damage < defence_power { 0 } else { damage - defence_power }
+        }
     };
     // Logging
     {
@@ -36,13 +48,6 @@ pub fn attack_neighbor(game: &mut Game, attacker: CharaId, target: CharaId) {
     }
     // Damage processing
     super::chara::damage(game, target, damage, DamageKind::ShortRangeAttack);
-    // Exp processing
-    {
-        let attacker = game.gd.chara.get_mut(attacker);
-        if let Some(weapon_kind) = weapon_kind {
-            attacker.skills.add_exp(SkillKind::Weapon(weapon_kind), 1);
-        }
-    }
     // Animation pushing
     game.anim_queue.push_attack(game.gd.get_current_map().chara_pos(target).unwrap());
 }
@@ -91,33 +96,6 @@ fn get_weapon_kind(item: &ItemObject) -> WeaponKind {
     match item.kind {
         ItemKind::Weapon(kind) => kind,
         _ => unreachable!(),
-    }
-}
-
-struct WeaponData {
-    dice_n: i32,
-    dice_x: i32,
-    /// For hand attack, this is None
-    kind: Option<WeaponKind>,
-}
-
-fn get_weapon_data(chara: &Chara) -> WeaponData {
-    if let Some(weapon) = chara.equip.item(EquipSlotKind::ShortRangeWeapon, 0) {
-        let item_obj = gobj::get_obj(weapon.idx);
-        let kind = match item_obj.kind {
-            ItemKind::Weapon(kind) => kind,
-            _ => unreachable!(),
-        };
-        WeaponData {
-            dice_n: item_obj.dice_n.into(),
-            dice_x: item_obj.dice_x.into(),
-            kind: Some(kind),
-        }
-    } else {
-        WeaponData {
-            dice_n: 4, dice_x: 4,
-            kind: None,
-        }
     }
 }
 
