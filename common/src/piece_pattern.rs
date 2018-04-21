1d0c1323f7e13751b12cede916c91b24ffb9c021
T. Okubo <t.okubo.rx78+devel@gmail.com>
2018-04-21 16:11:48 +0900

    Add PieceImgObject trait

--- common/src/piece_pattern.rs
+++ common/src/piece_pattern.rs
@@ -2,6 +2,8 @@
 //! TilePatternFlag helps to search appropriate pattern.
 
 use array2d::*;
+use basic::{PIECE_SIZE, PIECE_SIZE_I};
+use obj::ImgObject;
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub struct PiecePatternFlags(pub u8);
@@ -75,6 +77,48 @@ fn get_corner_piece_pattern(ns: u8, between: u8, ew: u8) -> u8 {
         (true , false, true ) => 4,
         (true , true , false) => 2,
         (true , true , true ) => 0,
+    }
+}
+
+pub trait PieceImgObject: ImgObject {
+    fn n_pattern(&self) -> u8;
+    
+    /// Get rect of the specified piece
+    fn piece_rect(&self, i_pattern: u8, i_piece: u32, i_anim_frame: u32) -> (i32, i32, u32, u32) {
+        let n_frame = self.get_img().n_frame;
+        let n_pattern = self.n_pattern() as u32;
+        let n_anim_frame = n_frame / n_pattern;
+        let img_rect = self.img_rect_nth(n_anim_frame * i_pattern as u32 + i_anim_frame);
+
+        match i_piece {
+            0 => ( // Top left piece
+                img_rect.0,
+                img_rect.1,
+                PIECE_SIZE,
+                PIECE_SIZE),
+            1 => ( // Top right piece
+                img_rect.0 + PIECE_SIZE_I,
+                img_rect.1,
+                PIECE_SIZE,
+                PIECE_SIZE),
+            2 => ( // Bottom left piece
+                img_rect.0,
+                img_rect.1 + PIECE_SIZE_I,
+                PIECE_SIZE,
+                img_rect.3 - PIECE_SIZE),
+            3 => ( // Bottom right piece
+                img_rect.0 + PIECE_SIZE_I,
+                img_rect.1 + PIECE_SIZE_I,
+                PIECE_SIZE,
+                img_rect.3 - PIECE_SIZE),
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl PieceImgObject for ::obj::TileObject {
+    fn n_pattern(&self) -> u8 {
+        self.n_pattern
     }
 }
 
