b6397aee9b0e6ab47127ccf3960b285d5f0cfa52
T. Okubo <t.okubo.rx78+devel@gmail.com>
2018-09-04 23:55:08 +0900

    Add town.rs

---
+++ rules/src/town.rs
@@ -0,0 +1,9 @@
+
+/// Used for town simulation
+#[derive(Serialize, Deserialize)]
+pub struct Town {
+    /// The minimum number of shop items
+    pub min_shop_items: u32,
+    /// The maximum number of shop items
+    pub max_shop_items: u32,
+}
