4cf2afd29281b1063cb2d893e8919fea826422a8
T. Okubo <t.okubo.rx78+devel@gmail.com>
2019-05-13 20:43:18 +0900

    Add find_key() to InputConfig

--- rusted-ruins/src/config/input.rs
+++ rusted-ruins/src/config/input.rs
@@ -7,3 +7,21 @@ pub struct InputConfig {
     pub dialog: HashMap<String, Command>,
     pub targeting: HashMap<String, Command>,
 }
+
+impl InputConfig {
+    pub fn find_key(&self, command: Command) -> String {
+        let mut s = String::new();
+
+        for (k, c) in &self.normal {
+            if *c != command {
+                continue;
+            }
+
+            if !s.is_empty() {
+                s.push_str(",");
+            }
+            s.push_str(k);
+        }
+        s
+    }
+}
