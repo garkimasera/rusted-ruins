061ab675600683804c4b8d39b612cedef348fd88
T. Okubo <t.okubo.rx78+devel@gmail.com>
2020-03-14 22:06:12 +0900

    Use thiserror for MapLoadError

--- common/src/impl_filebox.rs
+++ common/src/impl_filebox.rs
@@ -1,8 +1,8 @@
 use crate::gamedata::Map;
 use filebox::*;
 use serde_cbor::{error::Error as SerdeError, from_reader, ser::to_writer_packed};
-use std::fmt::Display;
 use std::io::{Error as IoError, Read, Write};
+use thiserror::Error;
 
 impl WithId for Map {
     type Error = MapLoadError;
@@ -17,38 +17,10 @@ impl WithId for Map {
     }
 }
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum MapLoadError {
-    Io(IoError),
-    Serde(SerdeError),
-}
-
-impl Display for MapLoadError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            MapLoadError::Io(a) => write!(f, "{}", a),
-            MapLoadError::Serde(a) => write!(f, "{}", a),
-        }
-    }
-}
-
-impl std::error::Error for MapLoadError {
-    fn description(&self) -> &str {
-        match self {
-            MapLoadError::Io(e) => e.description(),
-            MapLoadError::Serde(e) => e.description(),
-        }
-    }
-}
-
-impl From<IoError> for MapLoadError {
-    fn from(a: IoError) -> MapLoadError {
-        MapLoadError::Io(a)
-    }
-}
-
-impl From<SerdeError> for MapLoadError {
-    fn from(a: SerdeError) -> MapLoadError {
-        MapLoadError::Serde(a)
-    }
+    #[error("io error")]
+    Io(#[from] IoError),
+    #[error("serde error")]
+    Serde(#[from] SerdeError),
 }
