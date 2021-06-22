80e67038f011dfda324514accdd205eb538f489f
Rémi Lauzier <remilauzier@protonmail.com>
2021-06-22 10:22:11 -0400

    Fix a clippy warning

--- rusted-ruins/src/sdltypeconv.rs
+++ rusted-ruins/src/sdltypeconv.rs
@@ -7,23 +7,23 @@ pub const CENTERING_POS: i32 = -999;
 /// Centering for the screen
 pub const CENTERING_POS_FOR_SCREEN: i32 = -1000;
 
-impl Into<Rect> for CfgRect {
-    fn into(self) -> Rect {
-        let x = if self.x == CENTERING_POS {
-            SCREEN_CFG.main_window.x + (SCREEN_CFG.main_window.w - self.w) as i32 / 2
-        } else if self.x == CENTERING_POS_FOR_SCREEN {
-            (SCREEN_CFG.screen_w - self.w) as i32 / 2
+impl From<CfgRect> for Rect {
+    fn from(value: CfgRect) -> Rect {
+        let x = if value.x == CENTERING_POS {
+            SCREEN_CFG.main_window.x + (SCREEN_CFG.main_window.w - value.w) as i32 / 2
+        } else if value.x == CENTERING_POS_FOR_SCREEN {
+            (SCREEN_CFG.screen_w - value.w) as i32 / 2
         } else {
-            self.x
+            value.x
         };
-        let y = if self.y == CENTERING_POS {
-            SCREEN_CFG.main_window.y + (SCREEN_CFG.main_window.h - self.h) as i32 / 2
-        } else if self.y == CENTERING_POS_FOR_SCREEN {
-            (SCREEN_CFG.screen_h - self.h) as i32 / 2
+        let y = if value.y == CENTERING_POS {
+            SCREEN_CFG.main_window.y + (SCREEN_CFG.main_window.h - value.h) as i32 / 2
+        } else if value.y == CENTERING_POS_FOR_SCREEN {
+            (SCREEN_CFG.screen_h - value.h) as i32 / 2
         } else {
-            self.y
+            value.y
         };
-        Rect::new(x, y, self.w, self.h)
+        Rect::new(x, y, value.w, value.h)
     }
 }
 
