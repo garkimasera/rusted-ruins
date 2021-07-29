8dfdd7336aedec85d35a1638d6a81cb0d784df17
T. Okubo <t.okubo.rx78+devel@gmail.com>
2021-07-29 21:37:22 +0900

    Add ItemAttr for container

--- common/src/gamedata/item_attr.rs
+++ common/src/gamedata/item_attr.rs
@@ -1,5 +1,6 @@
 use super::item::*;
 use super::time::{Duration, Time};
+use std::cmp::{Eq, Ord, Ordering, PartialEq, PartialOrd};
 
 /// Attributes for ItemObject.
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Serialize, Deserialize)]
@@ -36,10 +37,40 @@ pub enum ItemAttr {
     /// Data to generate the contents.
     /// Used to fix generated contents when this item is opened.
     ContentGen { level: u32, seed: u32 },
+    /// Items in the container
+    Container(ItemListContainer),
     /// Material of this item
     Material(MaterialName),
     /// For skill learning items
     SkillLearning(super::skill::SkillKind),
     /// Title for readable items
     Title(String),
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct ItemListContainer {
+    /// Inner item list
+    item_list: ItemList,
+    /// Id used for item ordering
+    id: u64,
+}
+
+impl PartialEq for ItemListContainer {
+    fn eq(&self, other: &ItemListContainer) -> bool {
+        self.id == other.id
+    }
+}
+
+impl Eq for ItemListContainer {}
+
+impl PartialOrd for ItemListContainer {
+    fn partial_cmp(&self, other: &ItemListContainer) -> Option<Ordering> {
+        Some(self.id.cmp(&other.id))
+    }
+}
+
+impl Ord for ItemListContainer {
+    fn cmp(&self, other: &ItemListContainer) -> Ordering {
+        self.id.cmp(&other.id)
+    }
 }
