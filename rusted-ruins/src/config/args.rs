ab444c1e66e7e92b2590d1ac97f16150ec3521bb
T. Okubo <t.okubo.rx78+devel@gmail.com>
2022-01-11 20:32:24 +0900

    Add language command option

--- rusted-ruins/src/config/args.rs
+++ rusted-ruins/src/config/args.rs
@@ -4,6 +4,12 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Args {
+    /// Language
+    #[clap(long)]
+    lang: Option<String>,
+    /// Second language
+    #[clap(long)]
+    second_lang: Option<String>,
     /// Use fixed random seed
     #[clap(long)]
     fix_rand: bool,
@@ -13,6 +19,13 @@ pub fn modify_config_by_args(mut config: Config) -> Config {
     let args = Args::parse();
 
     config.fix_rand = args.fix_rand;
+
+    if let Some(lang) = args.lang {
+        config.lang = lang;
+    }
+    if let Some(second_lang) = args.second_lang {
+        config.second_lang = second_lang;
+    }
 
     config
 }
